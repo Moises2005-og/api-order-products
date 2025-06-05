37723ca661eb54d21ef7503e8fab7d080f87666a
moises <moisessebastiao2005@gmail.com>
2025-06-06 00:42:30 +0100

    remove CORS

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from 'express';
 import publicRoutes from "./routes/public/index.js";
 import privateRoutes from "./routes/private/index.js";
 import auth from "./middleware/auth.js";
+import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
+app.use(cors()); 
 
 app.use("/", publicRoutes);
 app.use("/", auth, privateRoutes)
